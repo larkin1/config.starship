e058170e418601567340e41a17feda7ca556ab14
larkin1 <larkindunlop2@gmail.com>
2026-04-21 11:14:54 +1000

    Add init.zsh and catppuccin mocha syntax highlighting

---
+++ init.zsh
@@ -0,0 +1,10 @@
+autoload -Uz compinit
+compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+
+source "${0:A:h}/catppuccin_mocha-zsh-syntax-highlighting.zsh"
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+export STARSHIP_CONFIG="${0:A:h}/starship.toml"
+eval "$(starship init zsh)"
